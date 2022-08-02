6bfb91b05b0ff77a68c9961ae70efe409f77b7c1
noahbarclay <nb56@mahurangi.school.nz>
2022-08-03 11:38:06 +1200

    everything broken

--- pages/index.php
+++ pages/index.php
@@ -11,24 +11,23 @@
 <link rel="script" href="script.js" />
 <script src="https://kit.fontawesome.com/7b1535357d.js" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">  
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
     <!-- MDB -->
     <link rel="stylesheet" href="../css/mdb.min.css" />
     <!-- Custom styles -->
     <link rel="stylesheet" href="../css/carousel.css" />
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css"/>
 
     <body>
 
     <header>
 
     <?php include("nav.php");?>
-
-    <!--Carousel Slider-->
-  
-    <link rel="stylesheet" href="carousel_style.css">
 
     <!-- Carousel wrapper -->
     <div id="introCarousel" class="carousel slide carousel-fade shadow-2-strong" data-mdb-ride="carousel">
@@ -46,12 +45,12 @@
           <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
             <div class="d-flex justify-content-center align-items-center h-100">
               <div class="text-white text-center">
-                <h1 class="mb-3">Welcome to Pix Media</h1>
-                <h5 class="mb-4">Buy epic photos</h5>
-                <a class="btn btn-outline-light btn-lg m-2" href=""
+                <h1 class="mb-3">Welcome to Southern Paprika</h1>
+                <h5 class="mb-4">Best & free guide of responsive web design</h5>
+                <a class="btn btn-outline-light btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A"
                   role="button" rel="nofollow" target="_blank">Start tutorial</a>
-                <a class="btn btn-outline-light btn-lg m-2" href="prints.php"
-                  target="_blank" role="button">Go to prints page</a>
+                <a class="btn btn-outline-light btn-lg m-2" href="https://mdbootstrap.com/docs/standard/"
+                  target="_blank" role="button">Download MDB UI KIT</a>
               </div>
             </div>
           </div>
@@ -68,36 +67,25 @@
           </div>
         </div>
 
-          <!-- Single item -->
+        <!-- Single item -->
         <div class="carousel-item">
-          <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+          <div class="mask" style="
+                background: linear-gradient(
+                  45deg,
+                   rgba(196.4800089597702, 248.0000004172325, 86.9999948143959, 0.7) 
+                  0%,rgba(255, 98.01999628543854, 87.99999475479126, 0.7) 100%) ;
+                
+              ">
             <div class="d-flex justify-content-center align-items-center h-100">
               <div class="text-white text-center">
-                <h2>You can place here any content</h2>
-              </div>
-            </div>
-          </div>
-
-         <!-- Single item -->
-         <div class="carousel-item">
-          <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
-            <div class="d-flex justify-content-center align-items-center h-100">
-              <div class="text-white text-center">
-                <h2>You can place here any content 2</h2>
-              </div>
-            </div>
-          </div>     
-         <!-- Single item -->
-        <div class="carousel-item">
-          <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
-            <div class="d-flex justify-content-center align-items-center h-100">
-              <div class="text-white text-center">
-                <h2>You can place here any content</h2>
+                <h2>Why don't you try some quality fruit now?</h2>
+                <a class="btn btn-outline-light btn-lg m-2"
+                  href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="_blank" role="button">Shop Now</a>
               </div>
             </div>
           </div>
         </div>
-
+      </div>
       <!-- Inner -->
 
       <!-- Controls -->
@@ -110,12 +98,10 @@
         <span class="sr-only">Next</span>
       </a>
     </div>
-    
+    <!-- Carousel wrapper -->
+  </header>
   <!--Main Navigation-->
-  
-    <script type="text/javascript" src="../bootstrap 4/js/mdb.min.js"></script>
-    <!-- Custom scripts -->
-    <script type="text/javascript" src="../bootstrap 4/js/mdb.min.js"></script>
-</header>
+
+
 
 <?php include("footer.php");?>
