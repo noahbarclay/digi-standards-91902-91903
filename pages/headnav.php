b00fa6a5093b66db4dc190c104cbce41c16bf13a
noahbarclay <nb56@mahurangi.school.nz>
2022-08-11 08:44:36 +1200

    Carousel FIx + admin

---
+++ pages/headnav.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Pix Prints</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://kit.fontawesome.com/b901cbb01d.js" crossorigin="anonymous"></script>
+  <link rel="icon" type="../image/logo.png" href="../images/logo.png">
+  
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- MDB for Carousel -->
+    <link rel="stylesheet" href="css/mdb.min.css"/>
+    <!-- Carousel styles -->
+    <link rel="stylesheet" href="css/carousel_style.css">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"> -->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" /> -->
+
+    </head>
+
+    <!--Main Navigation-->
+
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript" src="js/script.js"></script>
+
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
+  <div class="container">
+    <a href="index.php"><img src="images/logo.png" alt="logo" style="width:55px;height:55px;"></a>
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="prints.php">Prints</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="admin.php">Admin</a>
+        </li>
+        <a href="login.php" class="btn btn-dark ms-3">Login</a>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
