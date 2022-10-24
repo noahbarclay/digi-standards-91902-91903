ad0c296b4a0467e4ceaeb015589dffbbc91bb677
noahbarclay <nb56@mahurangi.school.nz>
2022-10-25 12:40:49 +1300

    Working Version

--- admin/authenticate.php
+++ admin/authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'pix prints';
+$DATABASE_NAME = 'pix_prints';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
